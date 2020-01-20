92033c926876d8a18e955c4d2e95a2b000d5ddd4
fernandesleticia <leticiafernandes777@gmail.com>
2020-01-20 11:43:27 -0300

    creating pipeline for graphql rouet

--- lib/graphbanking_web/router.ex
+++ lib/graphbanking_web/router.ex
@@ -1,11 +1,14 @@
 defmodule GraphbankingWeb.Router do
   use GraphbankingWeb, :router
 
-  pipeline :api do
-    plug :accepts, ["json"]
+  pipeline :graphql do
+	  plug Graphql.Context  #custom plug written into lib/graphql_web/plug/context.ex folder
   end
 
-  scope "/api", GraphbankingWeb do
-    pipe_through :api
-  end
+  scope "/api" do
+    pipe_through(:graphql)  #pipeline through which the request have to be routed
+  
+    forward("/",  Absinthe.Plug, schema: GraphqlWeb.Schema)
+    forward("/graphiql", Absinthe.Plug.GraphiQL, schema: GraphqlWeb.Schema)
+  end  
 end
